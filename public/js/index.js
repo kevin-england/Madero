a7a0003d44363e562e681845b5323fd953eded44
Kevin England <kevinengland5051@gmail.com>
2017-11-20 12:02:54 -0800

    #3. Created JS function to have close button available for users in the mobile side-nav menu and also added the new class for the button on each of the HTML pages.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+    /*Navbar active class function to toggle active class*/
     $.each($('.nav-wrapper').find('li'), function() {
         $(this).toggleClass('active', 
             window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
     }); 
+
+    /*Materialize Functions*/
     $(".button-collapse").sideNav();
     $('.carousel').carousel();
+    
+    /*Side nav close button function*/
+    $('.sideNav-close').click(function() {
+      $('.button-collapse').sideNav('hide');
+    });
 });
-
