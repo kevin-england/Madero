50c8646440cc4d9e1ea696ca6ef77d584289f3ea
Kevin England <kevinengland5051@gmail.com>
2017-11-25 13:29:09 -0800

    Updating homepage banner image along with several media queries for smaller screen sizes

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.parallax').parallax();
+    $('.slider').slider();
 
     /*Side nav close button function*/
     $('.side-nav-close').click(function() {
