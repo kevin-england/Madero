017ce6dd0c0aebbdc38e6674ee88b6d3c5acf9ae
Kevin England <kevinengland5051@gmail.com>
2017-12-05 14:29:12 -0800

    #4. Adding custom jquery function to the index.js file to get Materialize Pushpin component working properly

--- public/js/index.js
+++ public/js/index.js
@@ -23,4 +23,15 @@ $(document).ready(function() {
     $('.blurb').click(function() {
         $('.tap-target').tapTarget('open');
     })
+
+    /*Materialize Pushpin Component*/
+    $('.pushpin').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+    });
+        
 });
